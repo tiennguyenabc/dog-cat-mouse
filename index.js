bbb67e273e5081f710d7ec26f62ca2f420ceb889
TienNguyen <01228129873a@gmaiil.com>
2020-04-05 17:33:03 +0700

    Cat eat mickey

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let Dog = require('./dog');
 let Cat = require('./cat');
+let Mouse = require('./mouse');
 let dog = new Dog("Tom");
 dog.sayHi();	
+
 let tom = new Cat(); 
 dog.eat(tom);
+
+let mickey = new Mouse();
+tom.eat(mickey);
 console.log(dog);
